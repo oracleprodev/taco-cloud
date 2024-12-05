d62616e5c745073ff87a52f57e5c470a248ab045
Hamid Reza <oracleprodev@gmail.com>
2024-12-05 17:33:15 +0330

    Project Initializing ....

---
+++ src/main/java/com/hamid/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.hamid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
